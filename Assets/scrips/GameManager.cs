5da532caf58e6d7cb79f2fa1f01bc84fc16648a3
Travis m <murdocktravis03@gmail.com>
2024-12-18 10:27:04 -0800

    added gun model and set up for trm
    set up for trm by adding scrips for enemy and game manager so i can be ready to work trm

---
+++ Assets/scrips/GameManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject enemyPrefab; 
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
