94af470085dbbf3dc924a1ef03e70ab5890bc1ed
Travis m <murdocktravis03@gmail.com>
2024-12-19 10:22:14 -0800

    day 8

--- Assets/scrips/enemycontroler.cs
+++ Assets/scrips/enemycontroler.cs
@@ -4,14 +4,10 @@ using UnityEngine;
 
 public class enemycontroler : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
     void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
         
